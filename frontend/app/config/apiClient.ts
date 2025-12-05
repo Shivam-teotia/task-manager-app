20a4ff159ee3955115fed63b2999fb207ec3c75a
Shivam-Teotia <shivamteotia200@gmail.com>
2025-12-05 23:18:24 +0530

    update next js version

--- frontend/app/config/apiClient.ts
+++ frontend/app/config/apiClient.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const backendUrl = process.env.BACKEND_URL;
+const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 export const api = axios.create({
     baseURL: backendUrl,
 });
